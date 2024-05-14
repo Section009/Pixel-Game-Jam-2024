5648b8e8f0def152474def8c680c9db8db5d235b
Elia N Poncia <twins.eg04@gmail.com>
2024-05-14 17:17:27 -0400

    added in backend stuff for animation and such

--- Scripts/PlayerManager.gd
+++ Scripts/PlayerManager.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 
 
 #class vars
+var direction_vector = Vector2.DOWN
+
 static var Pos : Vector2 = Vector2(0.0, 0.0)
 static var Oxygen : float = 120.0
 
@@ -12,10 +14,12 @@ static var Oxygen : float = 120.0
 var OxygenTimeUI : Label
 
 
+
 #startup
 func _ready():
 	position = Pos
 	OxygenTimeUI = get_node("UI/OxygenTime")
+	
 	pass 
 
 #frame-based update
@@ -29,6 +33,18 @@ func _process(delta):
 func _physics_process(delta):
 	get_input()
 	pass
+
+#functions that specifically regard animation on the player:
+func _get_direction_string(angle:float) -> String:
+	var angle_deg = round(rad_to_deg(angle))
+	if(angle_deg > -90.0 and angle_deg < 90.0):
+		return "Right"
+	return "Left"
+func _play_animation(animation_type:String) -> void:
+	var animation_player = $AnimationPlayer
+	var animation_name = animation_type + "_" + _get_direction_string(direction_vector.angle())
+	animation_player.play(animation_name)
+	
 
 
 #input
